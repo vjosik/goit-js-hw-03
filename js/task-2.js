684d2bd30e3dc6bac5dbecc035b8470ae170eeef
vjosik <108922728+vjosik@users.noreply.github.com>
2026-01-18 23:56:02 +0200

    hm1

---
+++ js/task-2.js
@@ -0,0 +1,10 @@
+function makeArray(firstArray, secondArray, maxLength) {
+  let result = firstArray.join(" ") + " " + secondArray.join(" ");
+  return result.split(" ", maxLength);
+}
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
